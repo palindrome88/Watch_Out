5d6d61b22741a9ad8136a647fedda7042803cf14
Ryan Brisentine <brisentine.ryan@gmail.com>
2018-07-15 14:46:25 -0500

    Resize sidebar to include search bar

--- src/components/sidebar.js
+++ src/components/sidebar.js
@@ -2,30 +2,34 @@ import React, { Component } from 'react'
 import { Button, Header, Icon, Image, Menu, Segment, Sidebar } from 'semantic-ui-react'
 import { Map, InfoWindow, Marker, GoogleApiWrapper } from "google-maps-react";
 import MapComponent from './MapComponent';
+import Search from './search';
 export default class SidebarExampleDimmed extends Component {
-  state = {
+  constructor(props){
+      super(props)
+      this.state = {
        visible: false,
        windowPane: 0,
     }
+    this.handleButtonClick.bind(this);
+}
 
-  handleButtonClick = () => this.setState({ visible: !this.state.visible })
-
-  handleSidebarHide = () => this.setState({ visible: false })
+handleButtonClick = () => this.setState({ visible: !this.state.visible })
+handleSidebarHide = () => this.setState({ visible: false })
 
   compositeFunction0 = () => {
-      console.log("Works with multiple things");
+      console.log("Panel MENU.");
       this.handleButtonClick();
       this.handleWindowPane0();
   }
 
   compositeFunction1 = () => {
-    console.log("Works with multiple things");
+    console.log("Panel SEARCH.");
     this.handleButtonClick();
     this.handleWindowPane1();
 }
 
 compositeFunction2 = () => {
-    console.log("Works with multiple things");
+    console.log("Panel PROFILE.");
     this.handleButtonClick();
     this.handleWindowPane2();
 }
@@ -40,12 +44,12 @@ compositeFunction2 = () => {
     const { activeItem } = this.state
     const { visible } = this.state
 
-    if(this.state.windowPane === 0){
+    if(this.state.windowPane === 0){ // MENU ----------
         return (
             <div>
               
               <Sidebar.Pushable as={Segment}>
-                WATCH OUT!
+                WATCH OUT! 0
                 <Sidebar
                   as={Menu}
                   animation='overlay'
@@ -88,12 +92,57 @@ compositeFunction2 = () => {
 
           
     }
-    if (this.state.windowPane === 1){
+    if (this.state.windowPane === 1){ // ---------------SEARCH
         return (
             <div>
               
               <Sidebar.Pushable as={Segment}>
-                WATCH OUT!
+                WATCH OUT! 1
+                <Sidebar
+                  as={Menu}
+                  animation='overlay'
+                  icon='labeled'
+                  inverted
+                  onHide={this.handleSidebarHide}
+                  vertical
+                  visible={visible}
+                  width='thin'
+                >
+                  <Menu.Item as='a'>
+                    <Search submit={this.handleButtonClick}></Search>
+                  </Menu.Item>
+                  <Menu.Item as='a'>
+                    <Icon name='bomb'onClick={this.handleButtonClick}  />
+                    Flooded Area
+                  </Menu.Item>
+                  <Menu.Item as='a'>
+                    <Icon name='umbrella' onClick={this.handleButtonClick} />
+                    Stranger Danger
+                  </Menu.Item>
+                </Sidebar>
+      
+                <Sidebar.Pusher dimmed={visible}>
+                  <Segment basic>
+                  <MapComponent>
+                      </MapComponent>
+                <Menu fluid widths={4}>
+                    <Menu.Item name='menu' active={activeItem === 'MENU '} onClick={this.compositeFunction0} />
+                    <Menu.Item name='search' active={activeItem === 'SEARCH'} onClick={this.compositeFunction1} />
+                    <Menu.Item name='profile' active={activeItem === 'PROFILE'} onClick={this.compositeFunction2} />
+                </Menu>
+                    
+                  </Segment>
+                </Sidebar.Pusher>
+              </Sidebar.Pushable>
+            </div>
+          )
+      }
+      if (this.state.windowPane === 2){ // ---------- PROFILE
+        return (
+            <div>
+              
+              <Sidebar.Pushable as={Segment}>
+                WATCH OUT! 2
                 <Sidebar
                   as={Menu}
                   animation='overlay'
